001ade0abfd56b1b143a7781ceab16fe38ebfd83
davepeyton <davepeytonmusic@gmail.com>
2021-07-06 14:23:45 +0000

    added for loops

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    shop = [
+        {'title': 'Bikes'},
+        {'title': 'Parts'},
+    ]
+    return render(request, 'shop/index.html', {
+        'show_shop': False,
+        'shop': shop
+    })
