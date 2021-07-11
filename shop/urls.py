e097a038daa45e7669cc73d27105ca32a27f746c
davepeyton <davepeytonmusic@gmail.com>
2021-07-11 12:17:44 +0000

    added int id

--- shop/urls.py
+++ shop/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from urls import views
+from shop import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name='index'),
-    path('<int:id>',views.detail,name='detail'),
+    path('', views.index,name='index'),
+    path('<int:id>', views.detail,name='detail'),
 ]
