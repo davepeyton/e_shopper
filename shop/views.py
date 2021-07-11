fd275f75a205671ec3486ede9b3276536cd076ee
davepeyton <davepeytonmusic@gmail.com>
2021-07-11 12:07:07 +0000

    added details.html and linked views

--- shop/views.py
+++ shop/views.py
@@ -16,7 +16,7 @@ def index(request):
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
-    return render(request, 'shop/index.html',{'product_objects':product_objects})
+    return render(request,'shop/index.html',{'product_objects':product_objects})
 
 def detail(request),id:
     product_object = Products_objects.get(id=id)
