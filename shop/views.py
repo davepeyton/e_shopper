c9e8ede43b6edc807570684cc61fe7a4889309d9
davepeyton <davepeytonmusic@gmail.com>
2021-07-12 14:12:03 +0000

    debuging

--- shop/views.py
+++ shop/views.py
@@ -16,8 +16,8 @@ def index(request):
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
-    return render(request,'shop/index.html',{'product_objects':product_objects})
+    return render(request, 'shop/index.html',{'product_objects':product_objects})
 
 def detail(request),id:
     product_object = Products_objects.get(id=id)
-    return render(request,'shop/detail.html',{'product_object':product_object}
+    return render(request, 'shop/detail.html',{'product_object':product_object}
